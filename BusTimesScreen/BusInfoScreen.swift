5537a188c43435cc0268e66f7693a488957564c0
AKA77AKA77 <g.77@virgin.net>
2018-11-15 11:32:31 +0000

    Created opening screen with UICollectionview.

---
+++ BusTimesScreen/BusInfoScreen.swift
@@ -0,0 +1,20 @@
+//
+//  BusInfoScreen.swift
+//  BusTimes
+//
+//  Created by Ade Adegoke on 15/11/2018.
+//  Copyright © 2018 AKA. All rights reserved.
+//
+
+import UIKit
+
+class BusInfoScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ BusTimesScreen/BusInfoScreenCell/BusInfoScreenCell.swift
@@ -0,0 +1,18 @@
+//
+//  BusInfoScreenCell.swift
+//  BusTimes
+//
+//  Created by Ade Adegoke on 15/11/2018.
+//  Copyright © 2018 AKA. All rights reserved.
+//
+
+import UIKit
+
+class BusInfoScreenCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
